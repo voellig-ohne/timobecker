c61d8256a075f1864c6f8eb46df55682de8af0e1
Timur Çelikel <me@timurcelikel.com>
2016-06-07 18:06:50 +0200

    separate path generation and finding duplicates to separate file

--- pages/index.js
+++ pages/index.js
@@ -5,56 +5,14 @@ import DocumentTitle from 'react-document-title'
 import { config } from 'config'
 import Logo from './_logo'
 import POINTS from './_points'
+import findPaths from './_findPaths'
 
 import 'style/main.less'
 
 export default class Index extends React.Component {
     render () {
-        const automatedOrders = []
-        const orders = generatePermutations(POINTS.length)
-        const ordersWithoutLoops = orders;
-
-        let duplicatesDisplay;
-
-        let ordersString = orders.map((order) => {
-            return order.join('')
-        })
-
-        const uniques = []
-
-        const groups = {}
-        const grouped = {}
-
-        while (ordersString.length !== 0) {
-            const ordering = ordersString[0]
-            const duplicates = []
-
-            let shifted = ordering
-
-            uniques.push(ordering.split(''))
-
-            for (let i = 0; i < ordering.length; i++) {
-                shifted = shiftOrder(shifted)
-                const shiftedReverse = shifted.split('').reverse().join('')
-
-                const doup = ordersString.splice(ordersString.indexOf(shifted), 1)[0]
-                const doupReverse = ordersString.splice(ordersString.indexOf(shiftedReverse), 1)[0]
-
-                groups[doup] = ordering
-                groups[doupReverse] = ordering
-
-                duplicates.push(doup)
-                duplicates.push(doupReverse)
-            }
-
-            grouped[ordering] = duplicates
-        }
-
-        function shiftOrder (order) {
-            const array = order.split('')
-            array.push(array.shift())
-            return array.join('')
-        }
+        const paths = findPaths(POINTS.length)
+        const uniques = paths.uniques
 
         const logos = uniques.map((order, index) => {
             const title = order.map((i) => {
@@ -85,33 +43,4 @@ export default class Index extends React.Component {
             </DocumentTitle>
         )
     }
-}
-
-// http://stackoverflow.com/questions/9960908/permutations-in-javascript
-function generatePermutations (length) {
-    const input = []
-
-    for (let i = 0; i < length; i++) {
-        input.push(i)
-    }
-
-    const permArr = []
-    const usedChars = []
-
-    function permute(input) {
-        let i, ch
-        for (i = 0; i < input.length; i++) {
-            ch = input.splice(i, 1)[0]
-            usedChars.push(ch)
-            if (input.length == 0) {
-                permArr.push(usedChars.slice());
-            }
-            permute(input)
-            input.splice(i, 0, ch)
-            usedChars.pop()
-        }
-        return permArr
-    };
-
-    return permute(input)
 }
