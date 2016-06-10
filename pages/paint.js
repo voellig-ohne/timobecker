c7fc8e61f3c880b415b18e36a8d2ae73a5d3c478
Timur Çelikel <me@timurcelikel.com>
2016-06-10 14:44:31 +0200

    paint: show list of all cominations and make them drawable

--- pages/paint.js
+++ pages/paint.js
@@ -8,15 +8,50 @@ import POINTS from './_points'
 import PATHS from './_renderedPaths7.json'
 
 import 'style/main.less'
+import './paint.less'
 
 export default class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            activePath: undefined
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(path) {
+        this.setState({activePath: path});
+    }
     render () {
+        const pathList = PATHS.uniques.map((path) => {
+            const boundClick = this.handleClick.bind(null, path)
+            const className = 'paint-list_item' + ((this.state.activePath === path) ? ' paint-list_item--active' : '')
+
+            return (
+                <li key={path}
+                        onClick={boundClick}
+                        className={className}>
+                    {path}
+                </li>
+            )
+        })
+
+        const activePath = (this.state.activePath) ? this.state.activePath.join('') : 'nothing'
 
         return (
             <DocumentTitle title={config.siteTitle}>
                 <div>
                     <h1>Malen, Timo.</h1>
+                    selected: {activePath}
 
+                    <Logo className="tb_logo"
+                            points={POINTS}
+                            order={this.state.activePath}
+                            size={200}
+                            margin={20}
+                            showLabels={false} />
+                    <ul className="paint-list">
+                        {pathList}
+                    </ul>
                 </div>
             </DocumentTitle>
         )
