cd025846f5637a3034bbf9800ba76a319041dce9
Timur Çelikel <me@timurcelikel.com>
2016-06-24 11:36:58 +0200

    first connect the dots version

---
+++ pages/connect.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import DocumentTitle from 'react-document-title'
+import { config } from 'config'
+import Logo from './_logo'
+import POINTS from './_points'
+import PATHS from './_renderedPaths7.json'
+
+import 'style/main.less'
+import './connect.less'
+
+export default class Index extends React.Component {
+    constructor() {
+        super()
+    }
+    render () {
+
+        return (
+            <DocumentTitle title={config.siteTitle}>
+                <div>
+                    <h1>Connect the dots</h1>
+                    <Logo className="tb_logo"
+                            points={POINTS}
+                            size={800}
+                            margin={20}
+                            showLabels={false} />
+                </div>
+            </DocumentTitle>
+        )
+    }
+}
