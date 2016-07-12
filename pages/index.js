4aa7b5d9a8d275b62c4a0a2a7642b79c4e63e761
Timur Çelikel <me@timurcelikel.com>
2016-07-12 18:49:21 +0200

    add logo to intro page

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,30 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import DocumentTitle from 'react-document-title'
 import { config } from 'config'
+import Logo from 'components/logo/logo'
+import POINTS from 'components/logo/points'
 
 import 'style/main.less'
 
 export default class Index extends React.Component {
+    reset () {
+        this.refs.logo.reset()
+    }
     render () {
         return (
             <DocumentTitle title={config.siteTitle}>
-                <div>
+                <div className="intro">
+                    <Logo className="tb_logo"
+                            points={POINTS}
+                            size={800}
+                            margin={20}
+                            showLabels={false}
+                            mode="connect"
+                            ref="logo"/>
+                    <button onClick={this.reset.bind(this)}
+                            className="intro-reset">
+                        neu <span className="english">new</span>
+                    </button>
                 </div>
             </DocumentTitle>
         )
