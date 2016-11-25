181699390a58a241673cc98d77a391a4e35bba44
Timur Çelikel <me@timurcelikel.com>
2016-11-25 18:40:57 +0100

    projectList: basic output

--- components/ProjectList/index.js
+++ components/ProjectList/index.js
@@ -2,15 +2,46 @@ import React from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Article from 'components/Article'
+import { filter, includes, flow, map } from 'lodash'
 
 import style from './style.module.less'
 
 export default class ProjectList extends React.Component {
     render () {
-        return (
-            <Article>
+
+        const currentPath = this.props.children.props.route.path
+        const pages = this.props.children.props.route.pages
+
+        const projects = flow(
+            (pages) => filter(pages, page => isProject(page, currentPath)),
+            (pages) => map(pages, Project)
+        )(pages)
+
+        const mainAdditions = (
+            <div>
+                <h2>Projekte</h2>
+                { projects }
+            </div>
+        )
+
+        return (            
+            <Article mainAddition={ mainAdditions }>
                 {this.props.children}
             </Article>
         )
     }
 }
+
+function isProject(page, currentPath) {
+    return page.path !== currentPath && includes(page.path, currentPath)
+}
+
+function Project( project ) {
+    const { path } = project
+
+    return (
+        <Link to={path} key={path} className={style.projectLink}>
+            { project.data.title }
+        </Link>
+    )
+} 
