970b4eecfa5756ee348ca72e5b93d20eddb70e7a
Timur Çelikel <me@timurcelikel.com>
2016-12-03 09:58:54 +0100

    force reload background images

--- components/ResponsiveImage/index.js
+++ components/ResponsiveImage/index.js
@@ -7,7 +7,7 @@ export default class ResponsiveImage extends React.Component {
         if (process.env.NODE_ENV !== 'production') {
             const path = location + source + '.jpg'
             return (
-                <img src={path} className={className} alt={alt} />
+                <img key={source} src={path} className={className} alt={alt} />
             )
         } else {
             const path = location.substr(1)
@@ -21,7 +21,8 @@ export default class ResponsiveImage extends React.Component {
             const srcSet = size2 + ' 1000w, ' + size3 + ' 1500w, ' + size4 + ' 2000w, ' + size5 + ' 2500w, ' + size6 + ' 3000w'
 
             return (
-                <img src={size2}
+                <img key={source}
+                        src={size2}
                         srcSet={srcSet}
                         alt={alt}
                         className={className}/>
