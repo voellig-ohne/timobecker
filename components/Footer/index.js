2eb45e85f1b4c5e38af7fc4504b2c1dad788a5f4
Timur Çelikel <me@timurcelikel.com>
2016-12-21 21:17:33 +0100

    Footer: additional credit to völlig ohne

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -12,41 +12,46 @@ export default class Footer extends React.Component {
         const nextPrevSame = prev && prev.data.title === next.data.title
 
         return (
-            <footer className={style.container}>
-                { prev && !nextPrevSame ? 
-                    <Link to={prev.path} className={style.nextPrev_link}>
-                        <div className={style.nextPrev_label}>
-                            vorheriges
-                        </div>            
-                        <div className={style.nextPrev_title}>
-                            {prev.data.title}
-                        </div>
-                    </Link> : null 
-                }
-                <div className={style.content}>
-                    <a href="https://www.facebook.com/timo.becker.54" className={style.icon} title="Facebook" target="_blank">
-                        <Illustration illustration="icons_facebook" />
-                    </a>
-                    <a href="mailto:mail@timobecker.com" className={style.icon} title="Mail">
-                        <Illustration illustration="icons_mail" />
-                    </a>
-                    <a href="http://volligohne.de/projekte/timobecker/" className={style.icon} title="Völlig Ohne" target="_blank">
-                        <Illustration illustration="icons_vo" />
-                    </a>
-                    <Link to="/impressum/" className={style.icon} title="Impressum / Kontakt">
-                        <Illustration illustration="icons_impressum" />
-                    </Link>
+            <footer className={style.container_outer}>
+                <div className={style.container}>
+                    { prev && !nextPrevSame ? 
+                        <Link to={prev.path} className={style.nextPrev_link}>
+                            <div className={style.nextPrev_label}>
+                                vorheriges
+                            </div>            
+                            <div className={style.nextPrev_title}>
+                                {prev.data.title}
+                            </div>
+                        </Link> : null 
+                    }
+                    <div className={style.content}>
+                        <a href="https://www.facebook.com/timo.becker.54" className={style.icon} title="Facebook" target="_blank">
+                            <Illustration illustration="icons_facebook" />
+                        </a>
+                        <a href="mailto:mail@timobecker.com" className={style.icon} title="Mail">
+                            <Illustration illustration="icons_mail" />
+                        </a>
+                        <a href="http://volligohne.de/projekte/timobecker/" className={style.icon} title="Völlig Ohne" target="_blank">
+                            <Illustration illustration="icons_vo" />
+                        </a>
+                        <Link to="/impressum/" className={style.icon} title="Impressum / Kontakt">
+                            <Illustration illustration="icons_impressum" />
+                        </Link>
+                    </div>
+                    { next && !nextPrevSame ?
+                        <Link to={next.path} className={style.nextPrev_link}>
+                            <div className={style.nextPrev_label}>
+                                nächstes
+                            </div>     
+                            <div className={style.nextPrev_title}>
+                                {next.data.title}
+                            </div>
+                        </Link> : null
+                    }
                 </div>
-                { next && !nextPrevSame ?
-                    <Link to={next.path} className={style.nextPrev_link}>
-                        <div className={style.nextPrev_label}>
-                            nächstes
-                        </div>     
-                        <div className={style.nextPrev_title}>
-                            {next.data.title}
-                        </div>
-                    </Link> : null
-                }
+                <div className={style.vo_hint}>
+                    website by <a href="https://volligohne.de">völlig ohne</a>
+                </div>
             </footer>
         )
     }
