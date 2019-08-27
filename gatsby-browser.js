3797f683334ae0c96a159d6076a2500b8b9333c5
Timur Çelikel <me@timurcelikel.com>
2019-08-27 16:19:56 +0200

    delete unused stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,33 +1,5 @@
-// https://github.com/scottnonnenberg/blog/blob/master/gatsby-browser.js
-
-let first = true;
-
-function getDuration() {
-    const start = window.start || new Date();
-    const now = new Date();
-    const difference = now.getTime() - start.getTime();
-
-    if (difference === 0) {
-        return null;
-    }
-
-    return difference;
-}
-
-exports.onRouteUpdate = state => {
-    window._paq = window._paq || [];
-
-    if (first) {
-        first = false;
-        window._paq.push(['trackEvent', 'javascript', 'load', 'duration', getDuration()]);
-        console.log(
-            'Curios how its done? We are using paper.js and gatsby.js.',
-            'Some behind the scenes: http://volligohne.de/projekte/timobecker/.',
-            'Check out the source code https://github.com/voellig-ohne/timobecker'
-        );
-    } else {
-        window._paq.push(['setCustomUrl', state.pathname]);
-        window._paq.push(['setDocumentTitle', state.pathname]);
-        window._paq.push(['trackPageView']);
-    }
-};
+console.log(
+    'Curios how its done? We are using paper.js and gatsby.js.',
+    'Some behind the scenes: http://volligohne.de/projekte/timobecker/.',
+    'Check out the source code https://github.com/voellig-ohne/timobecker'
+);
