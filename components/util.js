3c8ab44f211165db1c928134feb0c4df63b8ee86
Timur Çelikel <me@timurcelikel.com>
2020-12-10 18:02:48 +0100

    utils: remove warnings

--- components/util.js
+++ components/util.js
@@ -4,14 +4,19 @@ exports.slugify = function slugify(string) {
     const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
     const p = new RegExp(a.split('').join('|'), 'g');
 
-    return string
-        .toString()
-        .toLowerCase()
-        .replace(/\s+/g, '-') // Replace spaces with -
-        .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
-        .replace(/&/g, '-and-') // Replace & with 'and'
-        .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-        .replace(/\-\-+/g, '-') // Replace multiple - with single -
-        .replace(/^-+/, '') // Trim - from start of text
-        .replace(/-+$/, ''); // Trim - from end of text
+    return (
+        string
+            .toString()
+            .toLowerCase()
+            .replace(/\s+/g, '-') // Replace spaces with -
+            .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+            // eslint-disable-next-line
+            .replace(/&/g, '-and-') // Replace & with 'and'
+            // eslint-disable-next-line
+            .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+            // eslint-disable-next-line
+            .replace(/\-\-+/g, '-') // Replace multiple - with single -
+            .replace(/^-+/, '') // Trim - from start of text
+            .replace(/-+$/, '')
+    ); // Trim - from end of text
 };
