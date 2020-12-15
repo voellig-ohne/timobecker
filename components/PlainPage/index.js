0eaa7d5efda0a6f5ad39078d508b6c72b211a4fa
Timur Çelikel <me@timurcelikel.com>
2020-12-15 10:53:52 +0100

    plainpage: add navigation and meta tags

--- components/PlainPage/index.js
+++ components/PlainPage/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import Navigation from '../Navigation';
 import style from './style.module.less';
 
-export default function ({ children, title }) {
+export default function ({ children, title, path }) {
     return (
-        <div className={style.container}>
-            <div className={style.inner}>
-                <h1 className={style.title}>{title}</h1>
-                {children}
+        <>
+            <div className={style.container}>
+                <div className={style.inner}>
+                    <h1 className={style.title}>{title}</h1>
+                    {children}
+                </div>
             </div>
-        </div>
+
+            <Helmet title={`Timo Becker • ${title}`}>
+                <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+            </Helmet>
+            <Navigation currentPath={path} />
+        </>
     );
 }
